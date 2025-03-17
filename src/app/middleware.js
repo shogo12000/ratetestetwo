8b90535fb5e4406187c7fe4c5ca0336abbf3c687
sidney <shogo12000@hotmail.com>
2025-03-17 01:02:51 -0700

    fix login register

---
+++ src/app/middleware.js
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+ 
+export async function middleware(request) {
+  const cookieStore = await cookies();
+  const authCookie = cookieStore.get('auth_token')?.value;
+ 
+ 
+  if(!authCookie){ 
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+ 
+  const { token } = JSON.parse(authCookie);
+
+  const apiUrl = new URL('/api/verifytoken', request.url).toString();
+
+  console.log("verify token do app")
+  console.log(token);
+  console.log("token.........")
+  console.log(request.url);
+  console.log(apiUrl);
+  console.log("xxxxxxxxxxxxxxxxxx")
+  const res = await fetch( apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  });
+  
+  console.log("res", res);
+  console.log("status --- ", res.status);
+
+  if(res.status != 200){
+   return NextResponse.redirect(new URL('/pageTwo', request.url))
+  }else{
+    return NextResponse.next();
+  }
+
+}
+
+export const config = {
+  matcher: ["/pageOne"],
+}
