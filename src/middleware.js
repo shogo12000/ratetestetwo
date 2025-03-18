5aff57cb7bea479b8c9af8d30e757d9f0e8c2ea0
sidney <shogo12000@hotmail.com>
2025-03-18 02:51:00 -0700

    page addReview

--- src/middleware.js
+++ src/middleware.js
@@ -11,28 +11,28 @@ export async function middleware(request) {
       return NextResponse.redirect(new URL('/', request.url));
     }
 
-    const { token } = JSON.parse(authCookie);
+    // const { token } = JSON.parse(authCookie);
 
-    const apiUrl = new URL('/api/verifytoken', request.url).toString();
+    // const apiUrl = new URL('/api/verifytoken', request.url).toString();
 
-    console.log(request.url);
-    console.log(apiUrl);
+    // console.log(request.url);
+    // console.log(apiUrl);
 
-    const res = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-    });
+    // const res = await fetch(apiUrl, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': `Bearer ${token}`,
+    //   },
+    // });
 
-    console.log("status --- ", res.status);
-
-    if (res.status !== 200) {
-      return NextResponse.redirect(new URL('/', request.url))
-    } else {
-      return NextResponse.next();
-    }
+    // console.log("status --- ", res.status);
+    return NextResponse.next();
+    // if (res.status !== 200) {
+    //   return NextResponse.redirect(new URL('/', request.url))
+    // } else {
+    //   return NextResponse.next();
+    // }
   } catch (error) {
     console.error('Erro no middleware:', error);
     return NextResponse.redirect(new URL('/', request.url));
