d468db65c5652bc60677df55dbb1d09a836a5418
sidney <shogo12000@hotmail.com>
2025-03-18 02:20:29 -0700

    page addReview

--- src/middleware.js
+++ src/middleware.js
@@ -1,46 +1,44 @@
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
- 
- 
+
 export async function middleware(request) {
-  const cookieStore = await cookies();
-  const authCookie = cookieStore.get('auth_token')?.value;
-  console.log("MIDDLEWARE..22222222222..")
- 
-  if(!authCookie){ 
+  try {
+    const cookieStore = cookies();
+    const authCookie = cookieStore.get('auth_token')?.value;
+    console.log("MIDDLEWARE..22222222222..")
+
+    if (!authCookie) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    const { token } = JSON.parse(authCookie);
+
+    const apiUrl = new URL('/api/verifytoken', request.url).toString();
+
+    console.log(request.url);
+    console.log(apiUrl);
+
+    const res = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+
+    console.log("status --- ", res.status);
+
+    if (res.status !== 200) {
+      return NextResponse.redirect(new URL('/', request.url))
+    } else {
+      return NextResponse.next();
+    }
+  } catch (error) {
+    console.error('Erro no middleware:', error);
     return NextResponse.redirect(new URL('/', request.url));
   }
- 
-  const { token } = JSON.parse(authCookie);
-
-  const apiUrl = new URL('/api/verifytoken', request.url).toString();
-
-  // console.log("verify token do app")
-  // console.log(token);
-  // console.log("token.........")
-  console.log(request.url);
-  console.log(apiUrl);
-  // console.log("xxxxxxxxxxxxxxxxxx")
-
-  const res = await fetch( apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  });
-  
-  // console.log("res", res);
-  console.log("status --- ", res.status);
-
-  if(res.status != 200){
-   return NextResponse.redirect(new URL('/', request.url))
-  }else{
-    return NextResponse.next();
-  }
-
 }
 
 export const config = {
-  matcher: ["/pageOne","/myReviews"],
+  matcher: ["/pageOne", "/myReviews"],
 }
