f13619851510612ba149dc6851a30fad45b205c7
sidney <shogo12000@hotmail.com>
2025-03-19 05:22:28 -0700

    page addReview

--- src/app/@auth/(.)userReview/page.js
+++ src/app/@auth/(.)userReview/page.js
@@ -1,16 +1,21 @@
 "use client";
-import { Modal } from "../../ui/modal"
-import { useSearchParams, useRouter } from "next/navigation";
-import UserReviewForm from "../../ui/userReviewForm"
- 
+
+import { Modal } from "../../ui/modal";
+import { useSearchParams } from "next/navigation";
+import UserReviewForm from "../../ui/userReviewForm";
+import { Suspense } from "react";
+
 export default function Page() {
   const searchParams = useSearchParams();
   const userId = searchParams.get("id");
+
   return (
-    <Modal> 
-      <h1>TEste</h1>
-      {userId}
-      <UserReviewForm userId={userId}/>
-    </Modal>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <Modal>
+        <h1>Teste</h1>
+        {userId}
+        <UserReviewForm userId={userId} />
+      </Modal>
+    </Suspense>
+  );
 }
